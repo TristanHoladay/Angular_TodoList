e523aaa43ba04671a29bf4056f8086f17a1c0c80
TristanHoladay <tholaday777@gmail.com>
2019-09-25 20:37:54 -0600

    Todo List App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+
+ ngOnInit() {
+   this.todoTitle = '';
+   this.todoList = [
+     { title: 'Install Angular CLI', isDone: false },
+   ];
+ }
+
+ addTodo():void {
+   this.todoList.push({
+     title: this.todoTitle,
+     isDone: false,
+   
+   });
+
+   this.todoTitle = '';
+ }
+
+  deleteTodo(todo:any) {
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+    this.todoList.splice(index, 1);
+  }
+
+  // markAsDone(todo:any, el:any) {
+  //   console.log(this);
+  //   todo.isDone = !todo.isDone;
+  // }
+
 }
